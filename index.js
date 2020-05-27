e3262704b3959a2618e1ce89fe28824ee8b3a3a1
filiafobico <luis.oliveira@quiver.local>
2020-05-27 20:50:10 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
 // config
 app.use(bodyParser.json({ limit: '100kb' }));
+app.use(cors());
 
 // routes
 app.use('/user', require('./routes/user'));
