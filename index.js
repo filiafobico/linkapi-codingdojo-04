9840a7c15fc3a9e6b2ca7900c9f93b486e0be92b
pedro <salgadopedroh@gmail.com>
2020-08-06 21:20:15 -0300

    Adicionado token jwt

--- index.js
+++ index.js
@@ -2,25 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const { isLoggedIn } = require('./middleware/passport/auth');
 
 // config
 app.use(bodyParser.json({ limit: '100kb' }));
 app.use(cors());
 
-// passport
-app.use(session({
-  secret: 'CoronaNaPrf@2020',
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // routes
 app.use('/auth', require('./routes/auth'));
-app.use('/user', isLoggedIn, require('./routes/user'));
+app.use('/user',  require('./routes/user'));
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/passport/auth');
+const User = require('../models/user/User');
+const bcrypt = require('bcryptjs');
+// const { auth } = require('../middleware/passport/auth');
+const jwt = require("jsonwebtoken");
 
-router.post('/', auth(), (req, res) => {
-  const { password, ...userWithoutPassword } = req.user[0];
-  res.status(200).json({
-    response: [userWithoutPassword],
-    error: null
-  });
+
+router.post('/', async (req, res) => {  
+  const users = await new User({}).getAll({ cpf: req.body.username });
+  const error = { response: [], error: "invalid username or password" };
+  if (Array.isArray(users) && users.length) {
+    if (bcrypt.compareSync(req.body.password, users[0].password)) {
+      const id = users[0].id;
+      const token = jwt.sign({ id }, "1q2w3e4r", {
+        expiresIn: 600 // expires in 5min
+      });
+      delete users[0].password;
+      res.status(200).json({
+        response: {token, user:users[0]},
+        error: null
+      });
+    } else {
+      res.status(200).json({
+        response: null,
+        error: 'Usuario invalido'
+      });
+    }
+  }
+  console.log(JSON.stringify(users));
+  
+
 });
 
 router.delete('/', (req, res) => {
